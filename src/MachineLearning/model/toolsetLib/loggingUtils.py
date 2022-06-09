b55f7fe4af97acec30df71ffdc910c63c5fa86cf
Albert Starfield <albertstarfield2001@gmail.com>
2022-06-10 06:48:02 +0700

    Action Segmentation Scrapped Unfortunately
    Moving to Image Recognition Method

--- src/MachineLearning/model/toolsetLib/loggingUtils.py
+++
@@ -1,9 +0,0 @@
-import logging as log
-#this is a utility function for logging
-def init_logger():
-    log.basicConfig(filename='log.txt', filemode='w',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=log.INFO, format='%(asctime)-15s %(message)s')
-    console = log.StreamHandler()
-    console.setLevel(log.INFO)
-    log.getLogger('').addHandler(console)
