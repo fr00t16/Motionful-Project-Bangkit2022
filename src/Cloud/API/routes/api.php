648b8e0a723365179f4fb42c9a61337052fb7783
Faizal Rifaldy <faizal.rifaldy@gmail.com>
2022-06-12 08:28:57 +0700

    add login api
    n/a

--- src/Cloud/API/routes/api.php
+++ src/Cloud/API/routes/api.php
@@ -8,3 +8,16 @@ use Illuminate\Support\Facades\Route;
  * @method "POST"
  */
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
